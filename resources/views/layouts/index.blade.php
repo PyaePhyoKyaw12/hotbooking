7c4cfa48f76dbf4cb59e07c47ab3837a0ad80cd7
PyaePhyoKyaw12 <pyaephyokyaw121318@gmail.com>
2024-12-23 11:15:49 +0630

    added file

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,277 @@
+@extends('layouts.master')
+
+@section('content')
+
+<body>
+
+
+    
+    <header role="banner">
+     
+      <nav class="navbar navbar-expand-md navbar-dark bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="index.html">LuxuryHotel</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse navbar-light" id="navbarsExample05">
+            <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+              <li class="nav-item">
+                <a class="nav-link active" href="index.html">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="rooms.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Rooms</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                  <a class="dropdown-item" href="rooms.html">Room Videos</a>
+                  <a class="dropdown-item" href="rooms.html">Presidential Room</a>
+                  <a class="dropdown-item" href="rooms.html">Luxury Room</a>
+                  <a class="dropdown-item" href="rooms.html">Deluxe Room</a>
+                </div>
+
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog.html">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+              </li>
+
+               <li class="nav-item cta">
+                <a class="nav-link" href="booknow.html"><span>Book Now</span></a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- END header -->
+
+    
+
+    <section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/big_image_1.jpg);">
+      <div class="container">
+
+      
+
+        <div class="row align-items-center site-hero-inner justify-content-center">
+
+        <section class="section bg-light pb-0  roomsserach" style="border-radius :15px"  >
+      <div class="container p-4 ">
+       
+        <div class="row check-availabilty" id="next">
+          <div class="block-32" data-aos="fade-up" data-aos-offset="-200">
+
+            <form action="#" class="ml-3 mr-3">
+              <div class="row">
+                <div class="col-md-6 mb-3 mb-lg-0 col-lg-3">
+                  <label for="checkin_date" class="font-weight-bold text-black">Check In</label>
+                  <div class="field-icon-wrap">
+                    <div class="icon"><span class="icon-calendar"></span></div>
+                    <input type="date" id="checkin_date" class="form-control">
+                  </div>
+                </div>
+                <div class="col-md-6 mb-3 mb-lg-0 col-lg-3">
+                  <label for="checkout_date" class="font-weight-bold text-black">Check Out</label>
+                  <div class="field-icon-wrap">
+                    <div class="icon"><span class="icon-calendar"></span></div>
+                    <input type="text" id="checkout_date" class="form-control">
+                  </div>
+                </div>
+
+                <div class="col-md-6 mb-3 mb-lg-0 col-lg-3">
+                      <label for="adults" class="font-weight-bold text-black">Room Type</label>
+                      <div class="field-icon-wrap">
+                      <div class="icon"><span class="icon-calendar"></span></div>
+                      <input type="text" id="checkout_date" class="form-control">
+                      </div>  
+                </div>
+
+                <div class="col-md-6 col-lg-3 align-self-end">
+                  <button class="btn btn-primary btn-block text-white">Check</button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
+       
+    
+
+          <div class="col-md-12 text-center">
+
+            <div class="mb-5 element-animate">
+              <h1>Welcome To Our Luxury Rooms</h1>
+              <p>Discover our world's #1 Luxury Room For VIP.</p>
+              <p><a href="booknow.html" class="btn btn-primary">Book Now</a></p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+   
+
+    <section class="site-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <div class="heading-wrap text-center element-animate">
+              <h4 class="sub-heading">Stay with our luxury rooms</h4>
+              <h2 class="heading">Stay and Enjoy</h2>
+              <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus illo similique natus, a recusandae? Dolorum, unde a quibusdam est? Corporis deleniti obcaecati quibusdam inventore fuga eveniet! Qui delectus tempore amet!</p>
+              <p><a href="#" class="btn btn-primary btn-sm">More About Us</a></p>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-7">
+            <img src="images/f_img_1.png" alt="Image placeholder" class="img-md-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+    <section class="site-section bg-light">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-12 heading-wrap text-center">
+            <h4 class="sub-heading">Our Luxury Rooms</h4>
+              <h2 class="heading">Featured Rooms</h2>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-md-7">
+            <div class="media d-block room mb-0">
+              <figure>
+                <img src="images/img_1.jpg" alt="Generic placeholder image" class="img-fluid">
+                <div class="overlap-text">
+                  <span>
+                    Featured Room 
+                    <span class="ion-ios-star"></span>
+                    <span class="ion-ios-star"></span>
+                    <span class="ion-ios-star"></span>
+                  </span>
+                </div>
+              </figure>
+              <div class="media-body">
+                <h3 class="mt-0"><a href="#">Presidential Room</a></h3>
+                <ul class="room-specs">
+                  <li><span class="ion-ios-people-outline"></span> 2 Guests</li>
+                  <li><span class="ion-ios-crop"></span> 22 ft <sup>2</sup></li>
+                </ul>
+                <p>Nulla vel metus scelerisque ante sollicitudin. Fusce condimentum nunc ac nisi vulputate fringilla. </p>
+                <p><a href="#" class="btn btn-primary btn-sm">Book Now From $20</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 room-thumbnail-absolute">
+            <a href="#" class="media d-block room bg first-room" style="background-image: url(images/img_2.jpg); ">
+              <!-- <figure> -->
+                <div class="overlap-text">
+                  <span>
+                    Hotel Room 
+                    <span class="ion-ios-star"></span>
+                    <span class="ion-ios-star"></span>
+                    <span class="ion-ios-star"></span>
+                  </span>
+                  <span class="pricing-from">
+                    from $22
+                  </span>
+                </div>
+              <!-- </figure> -->
+            </a>
+
+            <a href="#" class="media d-block room bg second-room" style="background-image: url(images/img_4.jpg); ">
+              <!-- <figure> -->
+                <div class="overlap-text">
+                  <span>
+                    Hotel Room 
+                    <span class="ion-ios-star"></span>
+                    <span class="ion-ios-star"></span>
+                    <span class="ion-ios-star"></span>
+                  </span>
+                  <span class="pricing-from">
+                    from $22
+                  </span>
+                </div>
+              <!-- </figure> -->
+            </a>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+   
+    
+    <section class="section-cover" data-stellar-background-ratio="0.5" style="background-image: url(images/img_5.jpg);">
+      <div class="container">
+        <div class="row justify-content-center align-items-center intro">
+          <div class="col-md-9 text-center element-animate">
+            <h2>Relax and Enjoy your Holiday</h2>
+            <p class="lead mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto quidem tempore expedita facere facilis, dolores!</p>
+            <div class="btn-play-wrap"><a href="https://vimeo.com/channels/staffpicks/93951774" class="btn-play popup-vimeo "><span class="ion-ios-play"></span></a></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+    
+    <section class="site-section bg-light">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-12 heading-wrap text-center">
+            <h4 class="sub-heading">Our Blog</h4>
+              <h2 class="heading">Our Recent Blog</h2>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-md-4">
+            <div class="post-entry">
+              <img src="images/img_3.jpg" alt="Image placeholder" class="img-fluid">
+              <div class="body-text">
+                <div class="category">Rooms</div>
+                <h3 class="mb-3"><a href="#">New Rooms</a></h3>
+                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum deserunt illo quis similique dolore voluptatem culpa voluptas rerum, dolor totam.</p>
+                <p><a href="#" class="btn btn-primary btn-outline-primary btn-sm">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="post-entry">
+              <img src="images/img_6.jpg" alt="Image placeholder" class="img-fluid">
+              <div class="body-text">
+                <div class="category">News</div>
+                <h3 class="mb-3"><a href="#">New Staff Added</a></h3>
+                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum deserunt illo quis similique dolore voluptatem culpa voluptas rerum, dolor totam.</p>
+                <p><a href="#" class="btn btn-primary btn-outline-primary btn-sm">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="post-entry">
+              <img src="images/img_5.jpg" alt="Image placeholder" class="img-fluid">
+              <div class="body-text">
+                <div class="category">New Rooms</div>
+                <h3 class="mb-3"><a href="#">Big Rooms for All</a></h3>
+                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum deserunt illo quis similique dolore voluptatem culpa voluptas rerum, dolor totam.</p>
+                <p><a href="#" class="btn btn-primary btn-outline-primary btn-sm">Read More</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+@endsection
